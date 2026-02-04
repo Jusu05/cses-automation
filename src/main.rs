84b41a4ec8cd7dfcc84aabdc06036e678c5ab2e0
Juuso Kosonen <kosonenjusu@gmail.com>
2026-02-04 21:42:53 +0200

    fixed exiting

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,11 @@ use std::path::PathBuf;
 
 #[tokio::main]
 async fn main() {
+    std::panic::set_hook(Box::new(|_| {}));
     let path = PathBuf::from(".");
     start(path).await;
+    let rt = tokio::runtime::Runtime::new().unwrap();
+    rt.block_on(async {
+        tokio::signal::ctrl_c().await.ok();
+    });
 }
